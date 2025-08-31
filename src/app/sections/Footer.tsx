7996278b64825cd19c01227934c25f9ce502243b
ExitProtocol <exitproto@protonmail.com>
2025-08-31 21:07:12 +0200

    Optimisation

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -6,6 +6,7 @@ import SocialGit from '@/assets/github.svg';
 import Logo from '@/assets/EX-logo-dark.svg';
 import Image from 'next/image';
 import { Audiowide } from 'next/font/google';
+import Link from 'next/link';
 
 const audiowide = Audiowide({ weight: '400', subsets: ['latin'], display: 'swap' });
 
@@ -18,25 +19,25 @@ export const Footer = () => {
                     <span className={`${audiowide.className} inline-flex gap-1 items-center text-3xl`}>EXIT</span>
                 </div>
                 <nav className='flex flex-col md:flex-row md:justify-center gap-6 mt-6'>
-                    <a href="/about">About</a>
-                    <a href="/coresystem">Core System</a>
-                    <a href="/comments">Voices from the Shadows</a>
-                    {/*<a href="/roadmap">Roadmap</a>*/}
-                    <a href="/help">Signal Uplink</a>
+                    <Link href="/about">About</Link>
+                    <Link href="/coresystem">Core System</Link>
+                    <Link href="/comments">Voices from the Shadows</a>
+                    {/*<Link href="/roadmap">Roadmap</Link>*/}
+                    <Link href="/help">Signal Uplink</Link>
                 </nav>
                 <div className='flex justify-center gap-6 mt-6'>
-                    <a href="https://twitter.com/exitproto" target="_blank" rel="noopener noreferrer">
+                    <Link href="https://twitter.com/exitproto" target="_blank" rel="noopener noreferrer">
                         <Image src={SocialX} alt='X' className="brightness-0 invert" />
-                    </a>
-                    <a href="https://t.me/" target="_blank" rel="noopener noreferrer">
+                    </Link>
+                    <Link href="https://t.me/" target="_blank" rel="noopener noreferrer">
                         <Image src={SocialTele} alt='Telegram' className="brightness-0 invert" height={10} width={25}/>
-                    </a>
-                    <a href="https://discord.com/" target="_blank" rel="noopener noreferrer">
+                    </Link>
+                    <Link href="https://discord.com/" target="_blank" rel="noopener noreferrer">
                         <Image src={SocialDc} alt='Dc' className="brightness-0 invert" height={10} width={25}/>
-                    </a>
-                    <a href="https://github.com/ExitProtocol/Token" target="_blank" rel="noopener noreferrer">
+                    </Link>
+                    <Link href="https://github.com/ExitProtocol/Token" target="_blank" rel="noopener noreferrer">
                         <Image src={SocialGit} alt='Git' className="brightness-0 invert" height={10} width={25}/>
-                    </a>
+                    </Link>
                 </div>
                 <p className='mt-6'>&copy;  2025 InExitWeTrust Inc. All rights reserved.</p>
             </div>
